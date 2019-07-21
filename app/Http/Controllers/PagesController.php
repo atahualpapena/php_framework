5c88886ff03004d0469856b783772682309a7387
Atahualpa Pena <atahualpapena@thegoat.local>
2019-07-20 23:07:14 -0700

    published code

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+	public function index(){
+		$title = "Welcome to laravel!!!";
+		return view('pages.index')->with('title', $title);
+
+	}
+
+	public function about(){
+		$title = "About Us!!!!";
+		return view('pages.about')->with('title', $title);
+
+	}
+
+	public function services(){
+
+		$data = array(
+			'title' => 'services', 
+			'services' => ['Web Development', 'SEO', 'Web Design']
+		);
+
+		return view('pages.services')->with($data);
+
+	}
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+
+@extends('layout.app')
+
+@section('content')
+        <h1>{{$title}}</h1>
+        <p>This is the Laravel application from the laravel from scratch Youtube Series.</p>
+@endsection
